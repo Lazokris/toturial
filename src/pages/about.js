b71609553ec2d3fac0236552da9a858e5b89339f
Kristian Lazo <56580883+Lazokris@users.noreply.github.com>
2022-08-10 13:17:35 -1000

    changes

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AboutPage = () => {
+    const message = 'Hi there! I\'m the proud creator of this site, which I built with Gatsby.'
+    return (
+        <main>
+            <title>About Me</title>
+            <h1>About Me</h1>
+            <p>{message}</p>
+        </main>
+    )
+}
+
+export default AboutPage
